8098f52f92b74e8a3696005385cc09f9de9c78dd
Steen Harsted <steenharsted@gmail.com>
2019-09-20 11:33:50 +0200

    update animate_movement()

--- R/animation_functions.R
+++ R/animation_functions.R
@@ -28,7 +28,30 @@
 #' @return Defaults to an animated gif. Different outputs can be achieved by passing different arguments via ... to the gganimate::animate() function. If return_plot = TRUE a ggplot plot is returned. If return_data = TRUE a tibble is returned.
 #' @export
 #'
-#' @examples\dontrun{}
+#' @examples
+#' # Prepare data
+#' df <- dplyr::filter(mocapr::mocapr_data, movement_nr == 1)
+#' df <- project_full_body_to_MP(df)
+#' df <- dplyr::filter(df, frame %in% c(1, 50, 100, 150, 200))
+#'
+#' # Return wrangled data
+#' mocapr::animate_movement(df, return_data = TRUE)
+#'
+#' # Return plot factted on frame
+#' mocapr::animate_movement(df,
+#'   return_plot = TRUE,
+#'   planes_in_rows_or_cols = "rows",
+#'   col_facets = frame
+#'   )
+#'
+#' # Same as above, but use ggforce::geom_circle()
+#' mocapr::animate_movement(df,
+#'   return_plot = TRUE,
+#'   use_geom_point = FALSE,
+#'   planes_in_rows_or_cols = "rows",
+#'   col_facets = frame
+#'   )
+#'
   animate_movement <- function(.data,
                                planes = c("R", "F"),
                                planes_in_rows_or_cols = c("cols"),
@@ -36,12 +59,12 @@
                                col_facets = NULL,
                                remove_facet_labels = TRUE,
                                use_geom_point = TRUE,
-                               line_colored_size = 2,
-                               line_black_size = 2,
+                               line_colored_size = 1,
+                               line_black_size = 1,
                                line_black_alpha = 0.5,
-                               point_size = 4,
+                               point_size = 1.5,
                                circle_size = 40,
-                               head_scale = 1.5,
+                               head_scale = 2,
                                torso_scale = 1.5,
                                return_data = FALSE,
                                return_plot = FALSE,
@@ -122,6 +145,7 @@
           Joint == "NC" ~ point_size*head_scale,
           Joint %in% c("LE", "RE") ~ point_size * 0.9,
           Joint %in% c("LW", "RW") ~ point_size * 0.8,
+          Joint %in% c("LK", "RK") ~ point_size * 0.95,
           Joint %in% c("LA", "RA") ~ point_size * 0.9,
           Joint %in% c("LT", "RT") ~ point_size * 0.8,
           TRUE ~ point_size),
@@ -130,6 +154,7 @@
           Joint == "NC" ~ circle_size*head_scale,
           Joint %in% c("LE", "RE") ~ circle_size * 0.9,
           Joint %in% c("LW", "RW") ~ circle_size * 0.8,
+          Joint %in% c("LA", "RA") ~ circle_size * 0.95,
           Joint %in% c("LA", "RA") ~ circle_size * 0.9,
           Joint %in% c("LT", "RT") ~ circle_size * 0.8,
           TRUE ~ circle_size),
@@ -144,20 +169,24 @@
     }
 
 
-      #Lets plot it!
-      ggplot2::ggplot(ggplot2::aes(group = Side, color = Side))+
-        ggplot2::ylab("Height (mm)")+
-        ggplot2::xlab("(mm)")+
-        ggplot2::coord_equal()+
-        ggplot2::guides(size = FALSE)+
-        ggplot2::theme_bw()+
-        ggplot2::theme(
-          axis.text.x = ggplot2::element_blank(),
-          axis.ticks.x = ggplot2::element_blank(),
-          axis.title.x = ggplot2::element_blank(),
-          legend.position = "bottom",
-          legend.title = ggplot2::element_blank())
+    #Lets plot it!
+    df_plot <- df_data %>%
+      ggplot2::ggplot(ggplot2::aes(group = Side_frame, color = Side))+
+      ggplot2::ylab("Height (mm)")+
+      ggplot2::xlab("(mm)")+
+      ggplot2::coord_equal()+
+      ggplot2::guides(size = FALSE)+
+      ggplot2::theme_bw()+
+      ggplot2::theme(
+        axis.text.x = ggplot2::element_blank(),
+        axis.ticks.x = ggplot2::element_blank(),
+        axis.title.x = ggplot2::element_blank(),
+        legend.position = "bottom",
+        legend.title = ggplot2::element_blank(),
+        panel.grid.major = ggplot2::element_blank())+
+      ggplot2::scale_size_identity()
 
+    # Remove facet labels
     if(remove_facet_labels){
       df_plot <- df_plot +
         ggplot2::theme(
@@ -165,6 +194,7 @@
           strip.text.y = ggplot2::element_blank())
     }
 
+    # How should the planes be faceted (make NULL if no facet is needed)
     if(planes_in_rows_or_cols == "rows"){
       rowplanes <- rlang::quo(Dir)
       colplanes <- NULL}
@@ -172,38 +202,41 @@
       rowplanes <- NULL
       colplanes <- rlang::quo(Dir)}
 
-    #Animation stuff
+    if( all(planes == "R") |  all(planes == "F")){
+      rowplanes <- NULL
+      colplanes <- NULL
+    }
+
+
+    #Animation and plotting stuff
+    # With geom_point
     if(use_geom_point){
-        df_plot <- df_plot +
-          ggplot2::geom_path(ggplot2::aes(x = value, y = U, color = Side, size = Size_Path))+
-          ggplot2::geom_point(ggplot2::aes(x = value, y = U, size = Size_Point))+
-          ggplot2::geom_path(ggplot2::aes(x = value, y = U), size = 0.75, color = "black")+
-          ggplot2::geom_point(ggplot2::aes(x = value, y = U), size = 1, color = "black")+
-          ggplot2::facet_grid(cols = dplyr::vars(!!colplanes, {{col_facets}}), rows = dplyr::vars(!!rowplanes, {{row_facets}}))
+      df_plot <- df_plot +
+        ggplot2::geom_path(ggplot2::aes(x = value, y = U, size = size_path_color))+
+        ggplot2::geom_point(ggplot2::aes(x = value, y = U, fill = Side, size = size_point), color = "black", shape = 21)+
+        ggplot2::geom_path(ggplot2::aes(x = value, y = U, size = size_path_black), color = "black", alpha = line_black_alpha)+
+        ggplot2::facet_grid(cols = dplyr::vars(!!colplanes, {{col_facets}}), rows = dplyr::vars(!!rowplanes, {{row_facets}}))
 
-        if(return_plot){return(df_plot)}
+      if(return_plot){return(df_plot)}
+      df_plot <- df_plot+
+        gganimate::transition_time(frame)+
+        gganimate::ease_aes('linear')
+      return(gganimate::animate(df_plot, ...))
+    }else{
+      # With geom_circle
+      df_plot <- df_plot +
+        ggplot2::geom_path(ggplot2::aes(x = value, y = U, size = size_path_color))+
+        ggforce::geom_circle(ggplot2::aes(x0 = value, y0 = U, r = size_circle, fill = Side), color = "black")+
+        ggplot2::geom_path(ggplot2::aes(x = value, y = U, size = size_path_black), color = "black", alpha = line_black_alpha)+
+        ggplot2::facet_grid(cols = dplyr::vars(!!colplanes, {{col_facets}}), rows = dplyr::vars(!!rowplanes, {{row_facets}}))
 
-        df_plot <- df_plot+
-          gganimate::transition_time(frame)+
-          gganimate::ease_aes('linear')
-        return(gganimate::animate(df_plot, ...))
-
-        }else{
-          df_plot <- df_plot +
-            ggforce::geom_circle(ggplot2::aes(x0 = value, y0 = U, r = Size_Point*12, fill = Side))+
-            ggplot2::geom_path(ggplot2::aes(x = value, y = U), size = 0.75, color = "black")+
-            ggforce::geom_circle(ggplot2::aes(x0 = value, y0 = U, r = 20), color = "black", fill = "black")+
-            ggplot2::facet_grid(cols = dplyr::vars(!!colplanes, {{col_facets}}), rows = dplyr::vars(!!rowplanes, {{row_facets}}))
-
-          if(return_plot){return(df_plot)}
-
-          df_plot <- df_plot+
-            gganimate::transition_time(frame)+
-            gganimate::ease_aes('linear')
-          return(gganimate::animate(df_plot, ...))
-      }
-}
-
+      if(return_plot){return(df_plot)}
+      df_plot <- df_plot+
+        gganimate::transition_time(frame)+
+        gganimate::ease_aes('linear')
+      return(gganimate::animate(df_plot, ...))
+    }
+  }
 
 ##Animate antomical (Function)----
 #' Animate motioncapture data in the anatomical planes
