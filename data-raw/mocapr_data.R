795ed97f20eabb3b037fa59aea573ac2b8321c07
Steen Harsted <steenharsted@gmail.com>
2019-10-11 15:15:04 +0200

    add new data

--- data-raw/mocapr_data.R
+++ data-raw/mocapr_data.R
@@ -7,25 +7,27 @@ mocapr_data <- tibble::tibble(
     file = stringr::str_match(file,  pattern = "(?<=shots_).*(?=_unknown)"),
     movement_description = dplyr::case_when(
       file == "Capoeira_1" ~ "capoeira dance",
-      file == "Gait_1_straight" ~ "normal gait in a straight line",
-      file == "Gait_2_square" ~ "normal gait in a semi square",
+      file == "Gait_1_straight" ~ "gait normal in a straight line",
+      file == "Gait_2_square" ~ "gait normal in a semi square",
       file == "Gait_3_drop_foot" ~ "gait with simulated drop foot",
       file == "Gait_4_int_rot" ~ "gait with simulated internal rotation",
       file == "SBJ_1" ~ "standing long jump for maximal performance",
-      file == "SBJ_2" ~ "standing long jump with simulated poor landing technique of the right lower extremity",
+      file == "SBJ_2" ~ "standing long jump for maximal performance",
+      file == "SBJ_3" ~ "standing long jump with simulated poor landing technique of the right lower extremity",
       file == "VJ_1" ~ "vertical jump for maximal performance",
       TRUE ~ "No information"
     )) %>%
   dplyr::mutate(
     movement_nr = dplyr::case_when(
-      file == "Capoeira_1" ~ 8,
-      file == "Gait_1_straight" ~ 4,
-      file == "Gait_2_square" ~ 5,
-      file == "Gait_3_drop_foot" ~ 6,
-      file == "Gait_4_int_rot" ~ 7,
+      file == "Capoeira_1" ~ 9,
+      file == "Gait_1_straight" ~ 5,
+      file == "Gait_2_square" ~ 6,
+      file == "Gait_3_drop_foot" ~ 7,
+      file == "Gait_4_int_rot" ~ 8,
       file == "SBJ_1" ~ 1,
       file == "SBJ_2" ~ 2,
-      file == "VJ_1" ~ 3,
+      file == "SBJ_3" ~ 3,
+      file == "VJ_1" ~ 4,
       TRUE ~ NA_real_
     )) %>%
 
@@ -36,7 +38,10 @@ mocapr_data <- tibble::tibble(
   # select, arrange, and unnest
   dplyr::select(movement_nr, movement_description, data) %>%
   dplyr::arrange(movement_nr) %>%
-  tidyr::unnest(cols = c(data))
+  tidyr::unnest(cols = c(data)) %>%
+
+  #remove the first part of movement_nr_2 as it is irrelevant
+  dplyr::filter(!(movement_nr == 3 & frame < 60))
 
 usethis::use_data(mocapr_data, overwrite = TRUE)
 
