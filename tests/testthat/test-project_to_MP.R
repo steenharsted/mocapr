21f74930e514424ac1726eb3afa2aef54b7cc6f4
Steen Harsted <steenharsted@gmail.com>
2019-10-22 21:38:39 +0200

    Set up alternative unit test

--- tests/testthat/test-project_to_MP.R
+++ tests/testthat/test-project_to_MP.R
@@ -1,4 +1,3 @@
-
 test_that("project_single_joint_to_MP fails when .method is not correct", {
   df <- dplyr::filter(mocapr::mocapr_data, movement_nr == 11)
   expect_error(project_single_joint_to_MP(df, LSX, LSY, LSZ, "LS", .method = 1))
@@ -6,14 +5,28 @@ test_that("project_single_joint_to_MP fails when .method is not correct", {
   expect_error(project_single_joint_to_MP(df, LSX, LSY, LSZ, "LS", .method = NA))
 })
 
-test_that("project_single_joint_to_MP works when .method is 'first_last'", {
+test_that("projecting to MP is stable when .method is 'first_last'", {
   df <- dplyr::filter(mocapr::mocapr_data, movement_nr == 11)
 
-  expect_known_value(project_single_joint_to_MP(df, LSX, LSY, LSZ, "LS", .method = "first_last"), here::here("tests", "testthat", "reference_files", "first_last"))
+  #expect_known_value(project_single_joint_to_MP(df, LSX, LSY, LSZ, "LS", .method = "first_last"), here::here("tests", "testthat", "reference_files", "first_last"))
+
+  df <- project_full_body_to_MP(df, .method = "first_last")
+  numeric_columns <- names(dplyr::select_if(df,is.numeric))
+  df <- tidyr::pivot_longer(df, cols = dplyr::one_of(numeric_columns))
+  df <- dplyr::summarise(df, value = mean(value))
+  expect_equal(round(dplyr::pull(df, value), 4), 160.5583)
+
 })
 
-test_that("project_single_joint_to_MP works when .method is 'first_dist'", {
+test_that("project to MP is stable when .method is 'first_dist'", {
   df <- dplyr::filter(mocapr::mocapr_data, movement_nr == 11)
-  expect_known_value(project_single_joint_to_MP(df, LSX, LSY, LSZ, "LS", .method = "first_dist"), here::here("tests", "testthat", "reference_files", "first_dist"))
+
+  #expect_known_value(project_single_joint_to_MP(df, LSX, LSY, LSZ, "LS", .method = "first_dist"), here::here("tests", "testthat", "reference_files", "first_dist"))
+
+  df <- project_full_body_to_MP(df, .method = "first_dist")
+  numeric_columns <- names(dplyr::select_if(df,is.numeric))
+  df <- tidyr::pivot_longer(df, cols = dplyr::one_of(numeric_columns))
+  df <- dplyr::summarise(df, value = mean(value))
+  expect_equal(round(dplyr::pull(df, value), 4), 202.4854)
 
 })
